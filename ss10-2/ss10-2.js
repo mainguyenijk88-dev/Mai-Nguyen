f82fd230308d826ace2817a16d96c6f689b5ba72
mainguyenijk88-dev <mainguyenijk88@gmail.com>
2025-11-05 14:11:02 +0900

    Add files via upload

---
+++ ss10-2/ss10-2.js
@@ -0,0 +1,10 @@
+
+let i = 10;
+let f = 20.5;
+let b = true;
+let s = "Ha Noi";
+// 
+document.writeln("i =" + i);
+document.writeln("f =" + f);
+document.writeln("b =" + b);
+document.writeln("s =" + s);
