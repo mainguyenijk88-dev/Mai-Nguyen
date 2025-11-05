ad245c60310a4fb74e24b77e4fb7742fd26469e3
mainguyenijk88-dev <mainguyenijk88@gmail.com>
2025-11-05 14:14:48 +0900

    Add files via upload

---
+++ ss10-4/ss10-4.js
@@ -0,0 +1,8 @@
+
+let price = 25000;
+
+let input = parseInt(prompt( "nhap so dola muon chuyen doi"))
+// document.writeln("ty gia chuyen doi"+"VND"+)
+
+let sum_convert = price*input
+document.writeln(sum_convert)
