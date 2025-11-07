e0297a304720b12ebadee9b6347951dbde396efb
mainguyenijk88-dev <mainguyenijk88@gmail.com>
2025-11-07 13:14:52 +0900

    Add files via upload

---
+++ bt1-ss11/bt1-ss11.js
@@ -0,0 +1,13 @@
+
+let year =prompt ( "vui long nhap nam:");
+if (year % 4==0 && year % 100!=0){
+alert("la nam nhuan");
+
+} else if(year % 400 ==0 )
+{
+  alert("la nam nhuan");
+}else
+{
+    alert("khong phai nam nhuan")
+}
+
