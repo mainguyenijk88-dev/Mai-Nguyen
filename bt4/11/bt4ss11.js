691e3322fc904dc0e48f63f8bccbc6653c433ed0
mainguyenijk88-dev <mainguyenijk88@gmail.com>
2025-11-07 10:29:28 +0900

    Add files via upload

---
+++ bt4/11/bt4ss11.js
@@ -0,0 +1,9 @@
+text=""
+for (i = 0; i <= 5; i=i+1) {
+   text += "The number is " + i + "\n";
+}
+console.log(text);
+
+// for (let i = 0; i<=5; i=i+1){
+//    console.log ( "The number is N")
+// }
